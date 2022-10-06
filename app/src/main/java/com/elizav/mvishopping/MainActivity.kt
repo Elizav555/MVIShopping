19d7b60ff8ef557e6f426ccfa9e6bc1226e86324
Elizaveta Garkina <lizagarkina5@gmail.com>
2022-10-06 16:39:20 +0300

    initial

---
+++ app/src/main/java/com/elizav/mvishopping/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.elizav.mvishopping
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.elizav.mvishopping.ui.theme.MVIShoppingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MVIShoppingTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MVIShoppingTheme {
+        Greeting("Android")
+    }
+}
