7516e0e18ef4c6f975938a32ca238ffb6ec043f8
Elizaveta Vitalievna <lizagarkina5@gmail.com>
2022-10-07 10:24:12 +0300

    move to views from compose

--- app/src/main/java/com/elizav/mvishopping/ui/navigation/Screen.kt
+++
@@ -1,9 +0,0 @@
-package com.elizav.mvishopping.ui.navigation
-
-import com.elizav.mvishopping.utils.Constants.AUTH_SCREEN
-import com.elizav.mvishopping.utils.Constants.PROFILE_SCREEN
-
-sealed class Screen(val route: String) {
-    object AuthScreen : Screen(AUTH_SCREEN)
-    object ProfileScreen : Screen(PROFILE_SCREEN)
-}
