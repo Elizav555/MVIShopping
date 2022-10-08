b39e08b745c3571e357582c0f75efc17e27535de
Elizaveta Vitalievna <lizagarkina5@gmail.com>
2022-10-08 18:46:39 +0300

    working on layout

--- app/src/main/java/com/elizav/mvishopping/ui/App.kt
+++ app/src/main/java/com/elizav/mvishopping/ui/App.kt
@@ -1,4 +1,4 @@
-package com.elizav.mvishopping
+package com.elizav.mvishopping.ui
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
---
+++ app/src/main/java/com/elizav/mvishopping/ui/products/ProductsListFragment.kt
@@ -0,0 +1,6 @@
+package com.elizav.mvishopping.ui.products
+
+import androidx.fragment.app.Fragment
+
+class ProductsListFragment: Fragment() {
+}
