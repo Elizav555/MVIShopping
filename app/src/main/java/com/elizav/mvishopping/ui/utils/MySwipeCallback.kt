15936553682860f5cafd83333f82773ddb59545d
Elizaveta Vitalievna <lizagarkina5@gmail.com>
2022-10-16 15:17:10 +0300

    disable drag

--- app/src/main/java/com/elizav/mvishopping/ui/utils/MySwipeCallback.kt
+++ app/src/main/java/com/elizav/mvishopping/ui/utils/MySwipeCallback.kt
@@ -18,9 +18,8 @@ abstract class MySwipeCallback(private val context: Context) : ItemTouchHelper.C
         recyclerView: RecyclerView,
         viewHolder: RecyclerView.ViewHolder
     ): Int {
-        val dragFlags = ItemTouchHelper.UP or ItemTouchHelper.DOWN
         val swipeFlag = ItemTouchHelper.START
-        return makeMovementFlags(dragFlags, swipeFlag)
+        return makeMovementFlags(0, swipeFlag)
     }
 
     override fun isLongPressDragEnabled(): Boolean = true
